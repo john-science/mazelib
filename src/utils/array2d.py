1aa0b9398eb3de1696d8dc9b0ee8e4ab5f5d8756
John Stilley <thejollysin@gmail.com>
2014-05-06 14:41:53 -0700

    Create array2d.py
    #2 Creating placeholder for the 2D array object.

---
+++ src/utils/array2d.py
@@ -0,0 +1,13 @@
+
+
+class Array2D(object):
+
+    def __init__(self):
+        self.height = 1
+        self.width = 1
+
+    def __str__(self):
+        return 'TODO: FIXME: This is a placeholder.'
+
+    def __repr__(self):
+        return self.__str__()
