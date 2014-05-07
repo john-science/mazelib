7292b0c74aee2c00fd8fe0159a382fa56ba7adec
theJollySin <thejollysin@gmail.com>
2014-05-06 21:21:57 -0700

    Added the Division maze gen algo.

--- __init__.py
+++ __init__.py
@@ -4,3 +4,4 @@ from src.utils.array2d import Array2D
 from src.generate.AldousBroder import AldousBroder
 from src.generate.BinaryTree import BinaryTree
 from src.generate.CellularAutomaton import CellularAutomaton
+from src.generate.Division import Division
