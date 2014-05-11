ec9511e3aadfb56e0d36dfc4aed6e72cb4d3525d
theJollySin <thejollysin@gmail.com>
2014-05-11 09:38:01 -0700

    improved tostring method to optionally print maze entrances

--- src/mazelib.py
+++ src/mazelib.py
@@ -9,45 +9,61 @@ class Maze(object):
         # TODO: Consider allowing grid/solution to be passed as optional parameters to Maze.
         self.generator = None
         self.grid = None
-        self.solver = None
-        self.solution = None
         self.start = None
         self.end = None
+        self.solver = None
+        self.solution = None
 
     def generate(self):
         self.grid = self.generator.generate()
 
-    def generate_random_entrances(self):
-        rows = len(self.grid)
-        cols = len(self.grid[0])
+    def generate_entrances(self):
+        # TODO: The entrances should have the option of being in three places: on the edges, just inside the edges, and randomly anywhere in the maze.
+        H = self.grid.height
+        W = self.grid.width
 
         start_side = randrange(4)
 
         if start_side == 0:
-            self.start = (0, randrange(1, cols, 2))         # North
-            self.end = (rows - 1, randrange(1, cols, 2))
+            self.start = (0, randrange(1, W, 2))         # North
+            self.end = (H - 1, randrange(1, W, 2))
         elif start_side == 1:
-            self.start = (rows - 1, randrange(1, cols, 2))  # South
-            self.end = (0, randrange(1, cols, 2))
+            self.start = (H - 1, randrange(1, W, 2))  # South
+            self.end = (0, randrange(1, W, 2))
         elif start_side == 2:
-            self.start = (randrange(1, rows, 2), 0)          # West
-            self.end = (randrange(1, rows, 2), cols - 1)
+            self.start = (randrange(1, H, 2), 0)          # West
+            self.end = (randrange(1, H, 2), W - 1)
         else:
-            self.start = (randrange(1, rows, 2), cols - 1)   # East
-            self.end = (randrange(1, rows, 2), 0)
+            self.start = (randrange(1, H, 2), W - 1)   # East
+            self.end = (randrange(1, H, 2), 0)
 
     def solve(self):
-        raise NotImplementedError('Please Implement this method.')
+        raise NotImplementedError('Please implement this method.')
 
     def tostring(self, entrances=False, solution=False):
         """Return a string representation of the maze."""
-        txt = ''
+        
+        # Build the walls of the grid
+        txt = []
         for row in self.grid:
+            txt_row = ''
             for cell in row:
-                txt += '#' if cell else ' '
-            txt += '\n'
+                txt_row += '#' if cell else ' '
+            txt.append(txt_row)
+        
+        # insert the start and end points
+        if entrances and self.start and self.end:
+            r,c = self.start
+            txt[r] = txt[r][:c] + 'S' + txt[r][c+1:]
+            r,c = self.end
+            txt[r] = txt[r][:c] + 'E' + txt[r][c+1:]
 
-        return txt
+        # if extant, insert the solution path
+        if solution and self.solution:
+            # TODO: Fill this in, after solve method is implemented.
+            pass
+
+        return '\n'.join(txt)
 
     def __str__(self):
         # TODO: At some ponit, this will include Entrances and solutions.
