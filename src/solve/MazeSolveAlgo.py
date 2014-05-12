a94a6fb40aa202fac9463fc17b863ab8bc46448c
John Stilley <thejollysin@gmail.com>
2014-05-12 14:49:25 -0700

    updating to make a clear API

--- src/solve/MazeSolveAlgo.py
+++ src/solve/MazeSolveAlgo.py
@@ -1,16 +1,10 @@
 
 import abc
-from ..utils.MazeArray import MazeArray
 
 
 class MazeSolveAlgo(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, grid, start, stop):
-        self.grid = grid
-        self.start = start
-        self.end = end
-
     @abc.abstractmethod
-    def solve(self):
+    def solve(self, grid, start, end):
         return
