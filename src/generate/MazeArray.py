09ab20ce053733a29cf126c0afd33a6ebed5ed5f
John Stilley <thejollysin@gmail.com>
2014-05-13 14:51:19 -0700

    removed duplicate class

--- src/generate/MazeArray.py
+++
@@ -1,8 +0,0 @@
-
-from ..utils.array2d import Array2D
-
-
-class MazeArray(Array2D):
-
-    def __init__(self, h, w, default_value=True):
-        super(MazeArray, self).__init__('b', (h,w), default_value)
