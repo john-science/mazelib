1da758403cda60901a88df29f34899df783e45a0
John Stilley <thejollysin@gmail.com>
2014-05-15 09:09:42 -0700

    cleanup of Wilson's algo

--- src/generate/Wilsons.py
+++ src/generate/Wilsons.py
@@ -11,16 +11,20 @@ class Wilsons(MazeGenAlgo):
     4. Repeat steps 2 and 3 until all cells have been added to the UST.
     """
 
-    def __init__(self, w, h, hunt_order='serpentine'):
+    def __init__(self, w, h, hunt_order='random'):
         super(Wilsons, self).__init__(w, h)
 
         # the user can define what order to hunt for the next cell in
         if hunt_order == 'random':
             self._hunt_order = self._hunt_random
-        else:
+        elif hunt_order == 'serpentine':
             self._hunt_order = self._hunt_serpentine
+        else:
+            self._hunt_order = self._hunt_random
 
     def generate(self):
+        raise NotImplementedError("Wilson's algorithm is not yet implemented.")
+
         grid = MazeArray(self.H, self.W)
         
         # find an arbitrary starting position
@@ -34,11 +38,37 @@ class Wilsons(MazeGenAlgo):
             if current == (-1, -1): break  # TODO: This is stupid. Fix the loop.
             walk = self._generate_random_walk(grid, current)
             self._solve_random_walk(grid, walk, current)
-            if num_trials == 1:
-                return grid
             num_trials += 1
 
         return grid
+
+    def _hunt(self, grid, count):
+        """ Based on how this algorithm was configured, choose hunt for the next starting point. """
+        return self._hunt_order(grid, count)
+
+    def _hunt_random(self, grid, count):
+        """ Select the next cell to walk from, randomly. """
+        if count >= (self.H * self.W + 2):
+            return (-1, -1)
+
+        return (randrange(1, self.H, 2), randrange(1, self.W, 2))
+
+    def _hunt_serpentine(self, grid, count):
+        """ Select the next cell to walk from by cycling through every grid cell in order. """
+        cell = (1, -1)
+        found = False
+
+        while not found:
+            cell = (cell[0], cell[1] + 2)
+            if cell[1] > (self.W - 2):
+                cell = (cell[0] + 2, 1)
+                if cell[0] > (self.H - 2):
+                    return (-1, -1)
+
+            if grid[cell] == 0 and len(self._find_neighbors(cell, grid, False)) > 0:
+                found = True
+
+        return cell
 
     def _generate_random_walk(self, grid, start):
         direction = self._random_dir(start)
@@ -84,38 +114,11 @@ class Wilsons(MazeGenAlgo):
         
         grid[((next_cell[0] + current[0]) // 2, (next_cell[1] + current[1]) // 2)] = 0
 
-    def _hunt(self, grid, count):
-        """ Based on how this algorithm was configured, choose hunt for the next starting point. """
-        return self._hunt_order(grid, count)
-
-    def _hunt_random(self, grid, count):
-        """ Select the next cell to walk from, randomly. """
-        if count >= (self.H * self.W + 2):
-            return (-1, -1)
-
-        return (randrange(1, self.H, 2), randrange(1, self.W, 2))
-
-    def _hunt_serpentine(self, grid, count):
-        """ Select the next cell to walk from by cycling through every grid cell in order. """
-        cell = (1, -1)
-        found = False
-
-        while not found:
-            cell = (cell[0], cell[1] + 2)
-            if cell[1] > (self.W - 2):
-                cell = (cell[0] + 2, 1)
-                if cell[0] > (self.H - 2):
-                    return (-1, -1)
-
-            if grid[cell] == 0 and len(self._find_neighbors(cell, grid, False)) > 0:
-                found = True
-
-        return cell
-
     # TODO: Several algorithms use this method, should they share it? (Add it to MazeGenAlgo?)
     def _find_neighbors(self, posi, grid, visited=True):  # TODO: Note, I changed this from 'unvisited' to 'visited'.  Fix this in Hunt-and-Kill
-        """ Find all the neighbors in the grid of the current position,
-        that have/haven't been visited.
+        """Find all the grid neighbors of the current position.
+        
+        Choose whether you want neighbors that Have or Haven't been visited already.
         """
         (row, col) = posi
         ns = []
