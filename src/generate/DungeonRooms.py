6ee93aeb4a1a69f9bea84b3dd06202958095329f
John Stilley <thejollysin@gmail.com>
2014-05-22 08:25:34 -0700

    redo Dungeon Rooms, 2b based on Hunt-and-Kill

--- src/generate/DungeonRooms.py
+++ src/generate/DungeonRooms.py
@@ -1,45 +1,103 @@
 
-from random import randrange,shuffle
-from MazeGenAlgo import MazeGenAlgo
+from random import choice,randrange,shuffle
+from MazeGenAlgo import MazeArray,MazeGenAlgo
 
 
 class DungeonRooms(MazeGenAlgo):
+    """
 
-    def __init__(self, grid):
+    0. Start with a grid which already includes open rooms.
+    1. Randomly choose a starting cell.
+    2. Perform a random walk from the current cel, carving passages to unvisited neighbors,
+    until the current cell has no unvisited neighbors.
+    3. Select a new grid cell; if it has been visited, walk from it.
+    4. Repeat steps 2 and 3 a sufficient number of times that there the probability of a cell
+    not being visited is extremely small.
+
+    This implementation of DungeonRooms is based on the classic Hunt-and-Kill algorithm.
+    
+    There are two different ways to select a new grid cell in step 2.
+    The first is serpentine through the grid (the classic solution), the second is to
+    randomly select a new cell enough times that the probability of anunexplored cell is
+    very, very low. The second option includes a small amount of risk, but it creates a
+    more interesting, harder maze.
+    """
+
+    def __init__(self, grid, hunt_order='random'):
         h = (grid.height - 1) // 2
         w = (grid.width - 1) // 2
         self.grid = grid
-        super(DungeonRooms, self).__init__(h, w)
+        super(DungeonRooms, self).__init__(w, h)
+
+        # the user can define what order to hunt for the next cell in
+        if hunt_order == 'random':
+            self._hunt_order = self._hunt_random
+        elif hunt_order == 'serpentine':
+            self._hunt_order = self._hunt_serpentine
+        else:
+            self._hunt_order = self._hunt_random
 
     def generate(self):
         current = self._choose_start()
         self.grid[current] = 0
+        
+        # find an arbitrary starting position
+        current = (randrange(1, self.H, 2), randrange(1, self.W, 2))
+        self.grid[current] = 0
 
-        # created a weighted list of all vertices connected in the graph
-        neighbors = self.find_neighbors(current, self.grid, False)
-
-        # loop over all current neighbors, until empty
-        visited = self._number_visited()
-        while visited < self.h * self.w:
-            # find neighbor with lowest weight, make it current
-            nn = randrange(len(neighbors))
-            current = neighbors[nn]
-            visited += 1
-            self.grid[current] = 0
-            neighbors = neighbors[:nn] + neighbors[nn + 1:]
-
-            # connect that neighbor to a random previously-visited neighbor
-            nearest_n = self.find_neighbors(current, self.grid)[0]
-            self.grid[(current[0] + nearest_n[0]) // 2, (current[1] + nearest_n[1]) // 2] = 0
-
-            # find all unvisited neighbors of current, add them to neighbors
-            unvisited = self.find_neighbors(current, self.grid, False)
-            for unv in unvisited:
-                if unv not in neighbors:
-                    neighbors.append(unv)
+        # perform many random walks, to fill the maze
+        num_trials = 0
+        while current != (-1, -1):
+            self._walk(self.grid, current)
+            current = self._hunt(self.grid, num_trials)
+            num_trials += 1
 
         return self.grid
 
+    def _walk(self, start):
+        """
+        This is a standard random walk. It must start from a visited cell.
+        And it completes when the current cell has no unvisited neighbors.
+        """
+        if self.grid[start] == 0:
+            current = start
+            unvisited_neighbors = self.find_neighbors(current, self.grid, False)
+
+            while len(unvisited_neighbors) > 0:
+                neighbor = choice(unvisited_neighbors)
+                self.grid[neighbor] = 0
+                self.grid[(neighbor[0] + current[0]) // 2, (neighbor[1] + current[1]) // 2] = 0
+                current = neighbor
+                unvisited_neighbors = self.find_neighbors(current, self.grid, False)
+
+    def _hunt(self, count):
+        """ Based on how this algorithm was configured, choose hunt for the next starting point. """
+        return self._hunt_order(self.grid, count)
+
+    def _hunt_random(self, count):
+        """ Select the next cell to walk from, randomly. """
+        if count >= (self.H * self.W):
+            return (-1, -1)
+
+        return (randrange(1, self.H, 2), randrange(1, self.W, 2))
+
+    def _hunt_serpentine(self, count):
+        """ Select the next cell to walk from by cycling through every grid cell in order. """
+        cell = (1, 1)
+        found = False
+
+        while not found:
+            cell = (cell[0], cell[1] + 2)
+            if cell[1] > (self.W - 2):
+                cell = (cell[0] + 2, 1)
+                if cell[0] > (self.H - 2):
+                    return (-1, -1)
+
+            if self.grid[cell] == 0 and len(self.find_neighbors(cell, self.grid, False)) > 0:
+                found = True
+
+        return cell
+        
     def _choose_start(self):
         """Choose a random starting location, that is not already inside a room.
         If no such room exists, the input grid was invalid.
@@ -58,19 +116,3 @@ class DungeonRooms(MazeGenAlgo):
             raise UnboundError('The grid input to DungeonRooms was invalid.')
 
         return current
-    
-    def _number_visited(self):
-        """Determine the number of grid cells already visited,
-        including those in the input grid rooms.
-        """
-        visited = 0
-
-        for r in xrange(1, self.H, 2):
-            for c in xrange(1, self.W, 2):
-                if self.grid[r,c] == 0:
-                    visited += 1
-
-        if visited >= self.h * self.h:
-            raise ValueError('The grid input to DungeonRooms was invalid.')
-
-        return visited
