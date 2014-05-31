8490026898af1098115cc8962d36f3a68030b9da
theJollySin <thejollysin@gmail.com>
2014-05-30 21:50:00 -0700

    exposing ShortestPath(s)

--- src/solve/DeadEndFiller.py
+++ src/solve/DeadEndFiller.py
@@ -29,11 +29,13 @@ class DeadEndFiller(MazeSolveAlgo):
         # loop through the maze serpentine, and find dead ends
         dead_end = self._find_dead_end()
         while dead_end != (-1, -1):
-            # from the dead end, travel one cell.
-            ns = self._find_neighbors(dead_end)
-
             # fill-in and wall-off the dead end
             self._fill_dead_end(dead_end)
+            
+            # from the dead end, travel one cell.
+            ns = self._find_unblocked_neighbors(dead_end)
+            
+            if len(ns) == 0: break
 
             # look at the next cell, if it is a dead end, restart the loop
             if len(ns) == 1:
@@ -43,7 +45,9 @@ class DeadEndFiller(MazeSolveAlgo):
                     continue
 
             # otherwise, find another dead end in the maze
-            dead_end = self._find_dead_end(s)
+            dead_end = self._find_dead_end()
+
+        print str(self.grid)
 
         solutions = self._build_solutions()
        
@@ -53,52 +57,70 @@ class DeadEndFiller(MazeSolveAlgo):
         """The maze generated has only solution cells.
         But the maze may have more than one solution. These need to be sorted out.
         """
-        # TODO: Will this be shared with other solvers?
+        # TODO: This will become the ShortestPaths solver.
         
         # find the starting positions
         start_posis = self._find_neighbors(self.start)
         
         if len(start_posis) == 0:
-            raise ValueError('No valid solutions found.')
+            raise ValueError('Input maze is invalid.')
         
         # 0. start a solution at each starting position
         solutions = []
+        
         for s in start_posis:
             solutions.append([s])
+        
+        # TODO: All of the self.end below are wrong. What if it is an edge start/end?
         
         num_unfinished = len(solutions)
         # 1. loop through each solution, and find the neighbors of the last element
         while num_unfinished > 0:
-            for s in len(solutions):
+            print(num_unfinished)
+            for s in xrange(len(solutions)):
                 if solutions[s][-1] in solutions[s][:-1]:
                     solutions[s].append(None)
-                elif solutions[s][-1] == start.end:
+                elif self._within_one(solutions[s][-1], self.end):
                     solutions[s].append(None)
                 elif solutions[s][-1] != None:
+                    if len(solutions[s]) > 1:
+                        # TODO: Document this: just a check to see if you've gone past the endpoint
+                        if self._midpoint(solutions[s][-1], solutions[s][-2]) == self.end:
+                            solutions[s].append(None)
+                            continue
                     # 2. since there are no dead ends, there can be 2, 3, or 4 neighbors
-                    ns = self._find_neighbors(solutions[s][-1])
+                    ns = self._find_unblocked_neighbors(solutions[s][-1])
                     if len(ns) in [0, 1]:
                         solutions[s].append(None)
-                    # 3. if 2, add the new cell to the colution
+                    # 3. if 2 neighbors, add the new cell to the colution
                     elif len(ns) == 2:
-                        if ns[0] == solutions[s][-1]:
+                        # TODO: Is this logic sound? What about the first move? Will the last cell always be in ns?
+                        # TODO: Review this logic. If we are at a dead-end, what happens?
+                        if len(solutions[s]) > 1 and ns[0] == solutions[s][-2]:
+                            solutions[s].append(self._midpoint(ns[1], solutions[s][-1]))
                             solutions[s].append(ns[1])
                         else:
+                            solutions[s].append(self._midpoint(ns[0], solutions[s][-1]))
                             solutions[s].append(ns[0])
                     # 4. if 3 or 4, 1 or 2 new solutions must be generated
                     else:
-                        ns.remove(solutions[s][-1])
-                        solutions[s].append(ns[0])
+                        # TODO: Review this logic. If we are at a dead-end, what happens?
+                        if len(solutions[s]) > 1:
+                            ns.remove(solutions[s][-3])
+                        
                         for j in xrange(1, len(ns)):
-                            solutions.append(list(solutions[s]).append(ns[j]))
-            
+                            if ns[j] not in solutions[s]:  # TODO: Necessary?
+                                solutions.append(list(solutions[s]) + [self._midpoint(ns[j], solutions[s][-1]), ns[j]])
+                        solutions[s].append(self._midpoint(ns[0], solutions[s][-1]))
+                        solutions[s].append(ns[0])
+
             # 5. a solution is done when we mark it by appending a None.
             num_unfinished = sum(map(lambda sol: 0 if sol[-1] is None else 1 , solutions))
-                            
+
         # 6. clean-up: remove incomplete solutions
         new_solutions = []
         for sol in solutions:
-            if len(sol) > 2 and sol[-1] = start.end:
+            if len(sol) > 2 and sol[-2] == self.end:
                 # remove end cell from solution
                 new_solutions.append(sol[:-2])
                 
@@ -106,6 +128,7 @@ class DeadEndFiller(MazeSolveAlgo):
         solutions = sorted(new_solutions, key=len)
         
         if len(solutions) == 0 or len(solutions[0]) == 0:
+            print self.start, self.end
             raise ValueError('No valid solutions found.')
 
         return solutions
@@ -115,11 +138,11 @@ class DeadEndFiller(MazeSolveAlgo):
         the walls around it.
         """
         r,c = dead_end
-        grid[r, c] = 1
-        grid[r - 1, c] = 1
-        grid[r + 1, c] = 1
-        grid[r, c - 1] = 1
-        grid[r, c + 1] = 1
+        self.grid[r, c] = 1
+        self.grid[r - 1, c] = 1
+        self.grid[r + 1, c] = 1
+        self.grid[r, c - 1] = 1
+        self.grid[r, c + 1] = 1
 
     def _find_dead_end(self):
         """A "dead end" is a cell with only zero or one open neighbors.
@@ -127,7 +150,7 @@ class DeadEndFiller(MazeSolveAlgo):
         """
         for r in xrange(1, self.grid.height, 2):
             for c in xrange(1, self.grid.width, 2):
-                if (n, c) in [self.start, self.end]:
+                if (r, c) in [self.start, self.end]:
                     continue
                 if self._is_dead_end((r, c)):
                     return (r, c)
@@ -137,16 +160,37 @@ class DeadEndFiller(MazeSolveAlgo):
     def _is_dead_end(self, cell):
         """A dead end has zero or one open neighbors."""
         ns = self._find_neighbors(cell)
-        if len(ns) in [0, 1]:
+        if self.grid[cell] == 1:
+            return False
+        elif len(ns) in [0, 1]:
             return True
         else:
             return False
 
-    def _find_neighbors(self, posi, visited=True):
-
+    def _find_unblocked_neighbors(self, posi, visited=True):
         """Find all the grid neighbors of the current position;
         visited, or not.
+        """
+        (row, col) = posi
+        ns = []
 
+        if row > 1 and self.grid[row-1, col] != visited and self.grid[row-2, col] != visited:
+            ns.append((row-2, col))
+        if row < self.grid.height-2 and self.grid[row+1, col] and self.grid[row+2, col] != visited:
+            ns.append((row+2, col))
+        if col > 1 and self.grid[row, col-1] and self.grid[row, col-2] != visited:
+            ns.append((row, col-2))
+        if col < self.grid.width-2 and self.grid[row, col+1] and self.grid[row, col+2] != visited:
+
+            ns.append((row, col+2))
+
+        shuffle(ns)
+
+        return ns
+
+    def _find_neighbors(self, posi, visited=True):
+        """Find all the grid neighbors of the current position;
+        visited, or not.
         """
         (row, col) = posi
         ns = []
@@ -197,3 +241,19 @@ class DeadEndFiller(MazeSolveAlgo):
     def _midpoint(self, a, b):
         """Find the wall cell between to passage cells"""
         return (a[0] + b[0]) // 2, (a[1] + b[1]) // 2
+
+    def _within_one(self, cell, desire):
+        """Is the current cell within one move of the desired cell?
+        Note, this might be one full more, or one half move.
+        """
+        if not cell or not desire:
+            return False
+        
+        if cell[0] == desire[0]:
+            if abs(cell[1] - desire[1]) < 2:
+                return True
+        elif cell[1] == desire[1]:
+            if abs(cell[0] - desire[0]) < 2:
+                return True
+        
+        return False
