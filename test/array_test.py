26c5287bb6a9f8d4e0033300216de566d6920364
John Stilley <thejollysin@gmail.com>
2014-06-05 11:04:15 -0700

    #36 adding unittests to utils

--- test/array_test.py
+++ test/array_test.py
@@ -1,4 +1,5 @@
 
+from array import array
 import unittest
 from mazelib.utils.array2d import Array2D
 from mazelib.utils.MazeArray import MazeArray
@@ -6,10 +7,44 @@ from mazelib.utils.MazeArray import MazeArray
 
 class ArrayTest(unittest.TestCase):
 
-    def testArray2D(self):
+    def testDefault(self):
         a = Array2D('b', (5, 5), True)
         self.assertTrue(a[0, 0])
         self.assertTrue(a[1, 1])
+
+    def testSize(self):
+        a = Array2D('b', (5, 5), True)
+        self.assertEqual(a.height, 5)
+        self.assertEqual(a.width, 5)
+        self.assertEqual(len(a), 25)
+
+    def testGetAndSetElement(self):
+        a = Array2D('b', (5, 5), True)
+        self.assertTrue(a[1,2])
+        a[1,2] = 0
+        self.assertFalse(a[1,2])
+
+    def testGetAndSetRow(self):
+        # create 2d array, defaulted to all True
+        dim = 5
+        a = Array2D('b', (dim, dim), True)
+        # test that all values are true
+        for col in a[0]:
+            self.assertTrue(col)
+        # set the first row to an all-false array
+        row = array('b', [0] *dim)
+        a[0] = row
+        # test that the first row was changed correctly
+        for col in a[0]:
+            self.assertFalse(col)
+    
+    """
+    TODO: Build new tests for:
+        set and get slice in array2d
+        create MazeArray
+        set and get element, row, and slice in MazeArray
+        experimental: set and get with tuples
+    """
 
 
 def main():
