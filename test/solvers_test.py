bbe59fc118cf38b710b71e3e5ec38f636cf3a325
John Stilley <thejollysin@gmail.com>
2014-06-05 14:46:33 -0700

    adding solver testing functionality

--- test/solvers_test.py
+++ test/solvers_test.py
@@ -7,7 +7,7 @@ from mazelib.mazelib import Maze
 
 class SolversTest(unittest.TestCase):
 
-    def within_one(self, cell1, cell2):
+    def _within_one(self, cell1, cell2):
         """Is one cell within one move of another?"""
         r1,c1 = cell1
         r2,c2 = cell2
@@ -19,28 +19,77 @@ class SolversTest(unittest.TestCase):
 
         return False
 
-    def duplicates_in_solution(self, solution):
+    def _duplicates_in_solution(self, solution):
         """No cell should appear twice in the same maze solution."""
         for i in xrange(len(solution[:-1])):
             if solution[i] in solution[i+1:]:
                 return True
 
         return False
-    
-    # TODO: Build test for one outer and one inner entrance
 
-    def testWallFollower(self):
+    def _create_maze_with_varied_entrances(self, start_outer=True, end_outer=True):
+        """create a maze with entrances inside/outside"""
         m = Maze()
         m.generator = Prims(4, 5)
         m.generate()
-        m.generate_entrances()
+
+        if start_outer and end_outer:
+            m.generate_entrances()
+        elif not start_outer and not end_outer:
+            m.generate_entrances(False)
+        else:
+            if start_outer:
+                m.start = (0, 3)
+                m.end = (5, 5)
+            else:
+                m.start = (5, 5)
+                m.end = (0, 3)
+
+        return m
+
+    def testWallFollowerOuterOuter(self):
+        """test against a maze with outer entrances"""
+        m = self._create_maze_with_varied_entrances(True, True)
         m.solver = WallFollower()
         m.solve()
 
         for sol in m.solutions:
-            self.assertFalse(self.duplicates_in_solution(sol))
-            self.assertTrue(self.within_one(m.start, sol[0]))
-            self.assertTrue(self.within_one(m.end, sol[-1]))
+            self.assertFalse(self._duplicates_in_solution(sol))
+            self.assertTrue(self._within_one(m.start, sol[0]))
+            self.assertTrue(self._within_one(m.end, sol[-1]))
+
+    def testWallFollowerInnerInner(self):
+        """test against a maze with inner entrances"""
+        m = self._create_maze_with_varied_entrances(False, False)
+        m.solver = WallFollower()
+        m.solve()
+
+        for sol in m.solutions:
+            self.assertFalse(self._duplicates_in_solution(sol))
+            self.assertTrue(self._within_one(m.start, sol[0]))
+            self.assertTrue(self._within_one(m.end, sol[-1]))
+
+    def testWallFollowerOuterInner(self):
+        """test against a maze with an outer start and an inner end"""
+        m = self._create_maze_with_varied_entrances(True, False)
+        m.solver = WallFollower()
+        m.solve()
+
+        for sol in m.solutions:
+            self.assertFalse(self._duplicates_in_solution(sol))
+            self.assertTrue(self._within_one(m.start, sol[0]))
+            self.assertTrue(self._within_one(m.end, sol[-1]))
+
+    def testWallFollowerInnerOuter(self):
+        """test against a maze with an inner start and an outer end"""
+        m = self._create_maze_with_varied_entrances(False, True)
+        m.solver = WallFollower()
+        m.solve()
+
+        for sol in m.solutions:
+            self.assertFalse(self._duplicates_in_solution(sol))
+            self.assertTrue(self._within_one(m.start, sol[0]))
+            self.assertTrue(self._within_one(m.end, sol[-1]))
 
 
 def main():
