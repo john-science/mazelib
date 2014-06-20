0de5327995e6edd450905f65a6a33eee079797b5
John Stilley <thejollysin@gmail.com>
2014-06-20 11:30:42 -0700

    #64 moving common methods to base class

--- mazelib/solve/BacktrackingSolver.py
+++ mazelib/solve/BacktrackingSolver.py
@@ -15,7 +15,7 @@ class BacktrackingSolver(MazeSolveAlgo):
         # a first move has to be made
         current = self.start
         if self._on_edge(self.start):
-            current = self._push_edge_start(self.start)
+            current = self._push_edge(self.start)
         solution.append(current)
 
         # pick a random neighbor and travel to it, until you're at the end
@@ -57,26 +57,6 @@ class BacktrackingSolver(MazeSolveAlgo):
 
         return (last_dir, current)
 
-    def _find_unblocked_neighbors(self, posi):
-        """Find all the grid neighbors of the current position;
-        visited, or not.
-        """
-        (row, col) = posi
-        ns = []
-
-        if row > 1 and self.grid[row-1, col] == False and self.grid[row-2, col] == False:
-            ns.append((row-2, col))
-        if row < self.grid.height-2 and self.grid[row+1, col] == False and self.grid[row+2, col] == False:
-            ns.append((row+2, col))
-        if col > 1 and self.grid[row, col-1] == False and self.grid[row, col-2] == False:
-            ns.append((row, col-2))
-        if col < self.grid.width-2 and self.grid[row, col+1] == False and self.grid[row, col+2] == False:
-            ns.append((row, col+2))
-
-        shuffle(ns)
-
-        return ns
-
     def _prune_solution(self, solution):
         """In the process of solving a maze, the algorithm might go down
         the wrong corridor then backtrack.
@@ -109,57 +89,3 @@ class BacktrackingSolver(MazeSolveAlgo):
             solution = solution[i+1:]
 
         return solution
-
-    def _on_edge(self, cell):
-        """Does the cell lay on the edge, rather inside of the maze grid?"""
-        r,c = cell
-
-        if r == 0 or r == self.grid.height - 1:
-            return True
-        if c == 0 or c == self.grid.width - 1:
-            return True
-
-        return False
-
-    def _push_edge_start(self, start):
-        """If you start on the edge of the maze,
-        you need to push in one cell.
-        This method assumes you start on the edge.
-        """
-        row,col = start
-
-        if row == 0:
-            return (1, col)
-        elif row == (self.grid.height - 1):
-            return (row - 1, col)
-        elif col == 0:
-            return (row, 1)
-        else:
-            return (row, col - 1)
-
-    def _move(self, start, direction):
-        """Convolve a position tuple with a direction tuple to
-        generate a new position.
-        """
-        return tuple(map(sum, zip(start, direction)))
-
-    def _midpoint(self, a, b):
-        """Find the wall cell between to passage cells"""
-        return (a[0] + b[0]) // 2, (a[1] + b[1]) // 2
-
-    def _within_one(self, cell, desire):
-        """Is the current cell within one move of the desired cell?
-        Note, this might be one full more, or one half move.
-        """
-        if not cell or not desire:
-            return False
-
-        rdiff = abs(cell[0] - desire[0])
-        cdiff = abs(cell[1] - desire[1])
-
-        if rdiff == 0 and cdiff < 2:
-            return True
-        elif cdiff == 0 and rdiff < 2:
-            return True
-
-        return False
