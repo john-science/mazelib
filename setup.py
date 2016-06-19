7715a255969d2f763fee420049805f0eea953c7b
John Stilley <thejollysin@gmail.com>
2016-06-18 22:37:32 -0700

    cythonizing huntandkill

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ ext_modules = [Extension("mazelib.utils.MazeArray", ["mazelib/utils/MazeArray.py
                Extension("mazelib.generate.DungeonRooms", ["mazelib/generate/DungeonRooms.pyx" ]),
                Extension("mazelib.generate.Ellers", ["mazelib/generate/Ellers.pyx" ]),
                Extension("mazelib.generate.GrowingTree", ["mazelib/generate/GrowingTree.pyx" ]),
+               Extension("mazelib.generate.HuntAndKill", ["mazelib/generate/HuntAndKill.pyx" ]),
                Extension("mazelib.generate.Kruskal", ["mazelib/generate/Kruskal.pyx" ]),
                Extension("mazelib.generate.Prims", ["mazelib/generate/Prims.pyx" ]),
                Extension("mazelib.generate.Sidewinder", ["mazelib/generate/Sidewinder.pyx" ])]
