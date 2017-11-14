0d89eb9fc8c7fe14ffea8949ef52f6329ec697d6
theJollySin <thejollysin@gmail.com>
2017-11-14 06:54:23 -0800

    #86 removing cython from setup.py

--- setup.py
+++ setup.py
@@ -6,44 +6,15 @@ Optionally, one developer reported having to set an environment variable to his 
 
     export CFLAGS="-I /usr/local/lib/python2.7/site-packages/numpy/core/include"
 
-To build the Cython extensions:
-    python setup.py build_ext --inplace
-
-You will need to have Cython installed, and the current version of mazelib is a little persnickety about the version:
-
-    pip install cython==0.21
-
 To build mazelib and install the package:
     python setup.py install
 '''
 
 from setuptools import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-cmdclass = {'build_ext': build_ext}
-ext_modules = [Extension("mazelib.generate.MazeGenAlgo", ["mazelib/generate/MazeGenAlgo.pyx" ]),
-               Extension("mazelib.generate.AldousBroder", ["mazelib/generate/AldousBroder.pyx" ]),
-               Extension("mazelib.generate.BacktrackingGenerator",
-                         ["mazelib/generate/BacktrackingGenerator.pyx" ]),
-               Extension("mazelib.generate.BinaryTree", ["mazelib/generate/BinaryTree.pyx" ]),
-               Extension("mazelib.generate.CellularAutomaton",
-                         ["mazelib/generate/CellularAutomaton.pyx" ]),
-               Extension("mazelib.generate.Division", ["mazelib/generate/Division.pyx" ]),
-               Extension("mazelib.generate.DungeonRooms", ["mazelib/generate/DungeonRooms.pyx" ]),
-               Extension("mazelib.generate.Ellers", ["mazelib/generate/Ellers.pyx" ]),
-               Extension("mazelib.generate.GrowingTree", ["mazelib/generate/GrowingTree.pyx" ]),
-               Extension("mazelib.generate.HuntAndKill", ["mazelib/generate/HuntAndKill.pyx" ]),
-               Extension("mazelib.generate.Kruskal", ["mazelib/generate/Kruskal.pyx" ]),
-               Extension("mazelib.generate.Perturbation", ["mazelib/generate/Perturbation.pyx" ]),
-               Extension("mazelib.generate.Prims", ["mazelib/generate/Prims.pyx" ]),
-               Extension("mazelib.generate.Sidewinder", ["mazelib/generate/Sidewinder.pyx" ]),
-               Extension("mazelib.generate.TrivialMaze", ["mazelib/generate/TrivialMaze.pyx" ]),
-               Extension("mazelib.generate.Wilsons", ["mazelib/generate/Wilsons.pyx" ])]
 
 
 setup(name='mazelib',
-    version='0.6',
+    version='0.7',
     description='A Python API for creating and solving mazes.',
     url='https://github.com/theJollySin/mazelib',
     author='John Stilley',
@@ -57,6 +28,7 @@ setup(name='mazelib',
                  'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
                  'Natural Language :: English',
                  'Topic :: Software Development :: Libraries :: Python Modules'],
     license='GPLv3',
@@ -67,10 +39,7 @@ setup(name='mazelib',
     platforms='any',
     test_suite="test",
     install_requires=[
-        "cython",
         "numpy",
     ],
-    zip_safe=False,
-    cmdclass = cmdclass,
-    ext_modules=ext_modules)
+    zip_safe=False)
 
