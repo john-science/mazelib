e369448b04fde356b96c014715da43cb2659bbaa
theJollySin <thejollysin@gmail.com>
2018-02-25 17:25:29 -0800

    #86 minor progress on cell auto

--- mazelib/generate/CellularAutomaton.py
+++ mazelib/generate/CellularAutomaton.py
@@ -2,15 +2,16 @@
 from mazelib.generate.MazeGenAlgo import MazeGenAlgo
 from mazelib.generate.MazeGenAlgo import np
 from random import choice, randrange
+from numpy.random import shuffle
 
 
 class CellularAutomaton(MazeGenAlgo):
-    """Cells survive if they have one to four neighbours.
-    If a cell has exactly three neighbours, it is born.
+    """ Cells survive if they have one to four neighbours.
+        If a cell has exactly three neighbours, it is born.
 
-    It is similar to Conway's Game of Life in that patterns
-    that do not have a living cell adjacent to 1, 4, or 5 other
-    living cells in any generation will behave identically to it.
+        It is similar to Conway's Game of Life in that patterns
+        that do not have a living cell adjacent to 1, 4, or 5 other
+        living cells in any generation will behave identically to it.
     """
 
     def __init__(self, w, h, complexity=1.0, density=1.0):
@@ -29,7 +30,7 @@ class CellularAutomaton(MazeGenAlgo):
 
         # adjust complexity and density relative to maze size
         if self.complexity <= 1.0:
-            self.complexity = self.complexity * (5.0 * (self.H + self.W))
+            self.complexity = self.complexity * 10 * (self.H + self.W)
         if self.density <= 1.0:
             self.density = self.density * (self.h * self.w)
 
@@ -39,17 +40,14 @@ class CellularAutomaton(MazeGenAlgo):
             grid[y][x] = 1
             for j in range(int(self.complexity)):
                 neighbors = self._find_neighbors(y, x, grid, True)  # is wall
-                neighbors += self._find_neighbors(y, x, grid)       # is open
-                if len(neighbors):
+                if len(neighbors) > 0 and len(neighbors) < 5:
+                    neighbors = self._find_neighbors(y, x, grid)       # is open
+                    if not len(neighbors):
+                        continue
                     r,c = choice(neighbors)
                     if grid[r][c] == 0:
                         grid[r][c] = 1
                         grid[r + (y - r) // 2][c + (x - c) // 2] = 1
                         x, y = c, r
-
-        # ensure all corners are filled
-        for r in range(2, self.H - 2, 2):
-            for c in range(2, self.W - 2, 2):
-                grid[r][c] = 1
 
         return grid
