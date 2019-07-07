c194fb06d37612c4b7845ed0371ec8fcbc3dbd95
theJollySin <john_science@protonmail.com>
2019-07-07 14:30:49 -0400

    CLEANUP benchmark script was ugly

--- benchmarks.py
+++ benchmarks.py
@@ -12,17 +12,9 @@ standard basis for comparison.
 '''
 SIZES = [5, 10, 25, 50, 100, 200]
 ITERATIONS = [100, 100, 100, 50, 1, 1]
-GENERATORS = ['AldousBroder', 'AldousBroder',
-              'BacktrackingGenerator', 'BacktrackingGenerator',
-              'BinaryTree', 'BinaryTree',
-              'Division', 'Division',
-              'GrowingTree', 'GrowingTree',
-              'HuntAndKill', 'HuntAndKill',
-              'Prims', 'Prims',
-              'Sidewinder', 'Sidewinder',
-              'TrivialMaze', 'TrivialMaze',
-              'Wilsons', 'Wilsons']
-SOLVERS = ['Collision', 'WallFollower'] * int(len(GENERATORS) / 2)
+GENERATORS = ['AldousBroder', 'BacktrackingGenerator', 'BinaryTree', 'Division', 'GrowingTree',
+              'HuntAndKill', 'Prims', 'Sidewinder', 'TrivialMaze', 'Wilsons']
+SOLVERS = ['Collision', 'WallFollower']
 
 
 def main():
@@ -35,27 +27,30 @@ def run_benchmarks():
     An annoying screen-print will occur so that you know your
     progress, as these tests might take a while.
     '''
-    times = [[0.0]*len(SIZES) for _ in range(len(GENERATORS))]
+    times = [[0.0]*len(SIZES) for _ in range(len(GENERATORS) * len(SOLVERS))]
 
-    for row,generator in enumerate(GENERATORS):
-        solver = SOLVERS[row]
-        print('Run #%d: %s & %s' % (row, generator, solver))
-        for col,size in enumerate(SIZES):
-            print(col)
-            setup = """from mazelib import Maze
+    row = -1
+    for generator in GENERATORS:
+        for solver in SOLVERS:
+            row += 1
+            print('Run #%d: %s & %s' % (row, generator, solver))
+            for col, size in enumerate(SIZES):
+                print(col)
+                setup = """from mazelib import Maze
 from mazelib.solve.%(solv)s import %(solv)s
 from mazelib.generate.%(gen)s import %(gen)s
 """ % {'solv': solver, 'gen': generator}
-            logic = """m = Maze()
+                logic = """m = Maze()
 m.generator = %(gen)s(%(size)d, %(size)d)
 m.solver = %(solv)s()
 m.generate()
 m.generate_entrances()
 m.solve()
 """ % {'solv': solver, 'gen': generator, 'size': size}
-            t = Timer(logic, setup=setup)
-            time = t.timeit(ITERATIONS[col])
-            times[row][col] = time
+                t = Timer(logic, setup=setup)
+                time = t.timeit(ITERATIONS[col])
+                times[row]
+                times[row][col] = time
 
     return times
 
@@ -73,9 +68,13 @@ def print_benchmarks(times):
     print('\nTotal Time (seconds): %.5f\n' % sum([sum(times_row) for times_row in times]))
 
     print('\nmaze size,' + ','.join([str(s) for s in SIZES]))
-    for row in range(len(times)):
-        method = GENERATORS[row] + '-' + SOLVERS[row] + ','
-        print(method + ','.join(['%.5f' % time for time in times[row]]))
+
+    row = -1
+    for generator in GENERATORS:
+        for solver in SOLVERS:
+            row += 1
+            method = generator + '-' + solver + ','
+            print(method + ','.join(['%.5f' % time for time in times[row]]))
 
 
 if __name__ == '__main__':
