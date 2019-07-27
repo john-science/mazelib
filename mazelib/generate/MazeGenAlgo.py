be500a4e562c1a353a3ce1ec651c5901268acca1
theJollySin <john_science@protonmail.com>
2019-07-26 22:00:14 -0400

    PEP8 styling and better comments

--- mazelib/generate/MazeGenAlgo.py
+++ mazelib/generate/MazeGenAlgo.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.random import shuffle
 
 
-class MazeGenAlgo: 
+class MazeGenAlgo:
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, h, w):
@@ -24,18 +24,25 @@ class MazeGenAlgo:
 
     def _find_neighbors(self, r, c, grid, is_wall=False):
         """ Find all the grid neighbors of the current position; visited, or not.
+
+        Args:
+            r (int): row of cell of interest
+            c (int): column of cell of interest
+            grid (np.array): 2D maze grid
+            is_wall (bool): Are we looking for neighbors that are walls, or open cells?
+        Returns:
+            list: all neighboring cells that match our request
         """
         ns = []
 
-        if r > 1 and grid[r-2][c] == is_wall:
-            ns.append((r-2, c))
-        if r < self.H-2 and grid[r+2][c] == is_wall:
-            ns.append((r+2, c))
-        if c > 1 and grid[r][c-2] == is_wall:
-            ns.append((r, c-2))
-        if c < self.W-2 and grid[r][c+2] == is_wall:
-            ns.append((r, c+2))
+        if r > 1 and grid[r - 2][c] == is_wall:
+            ns.append((r - 2, c))
+        if r < self.H - 2 and grid[r + 2][c] == is_wall:
+            ns.append((r + 2, c))
+        if c > 1 and grid[r][c - 2] == is_wall:
+            ns.append((r, c - 2))
+        if c < self.W - 2 and grid[r][c + 2] == is_wall:
+            ns.append((r, c + 2))
 
         shuffle(ns)
-
         return ns
