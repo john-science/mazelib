b771a7056eab303d1d42da9d4bb9c047c110aba0
John Stilley <thejollysin@gmail.com>
2021-11-13 07:34:24 -0800

    Formatting using Python black

--- benchmarks.py
+++ benchmarks.py
@@ -7,9 +7,17 @@ from mazelib import __version__ as version
 # CONFIG
 SIZES = [5, 10, 25, 50, 100]
 ITERATIONS = [100, 50, 20, 5, 1]
-GENERATORS = ['AldousBroder', 'BacktrackingGenerator', 'BinaryTree', 'HuntAndKill', 'Prims', 'Sidewinder',
-              'TrivialMaze', 'Wilsons']
-SOLVERS = ['Collision', 'Tremaux']
+GENERATORS = [
+    "AldousBroder",
+    "BacktrackingGenerator",
+    "BinaryTree",
+    "HuntAndKill",
+    "Prims",
+    "Sidewinder",
+    "TrivialMaze",
+    "Wilsons",
+]
+SOLVERS = ["Collision", "Tremaux"]
 
 
 def main():
@@ -18,7 +26,7 @@ def main():
 
 
 def run_benchmarks():
-    """ Run the benchmarks.
+    """Run the benchmarks.
     An annoying screen-print will occur so that you know your progress, as these tests might take a while.
 
     Returns:
@@ -30,20 +38,27 @@ def run_benchmarks():
     for generator in GENERATORS:
         for solver in SOLVERS:
             row += 1
-            print('Run #%d: %s & %s' % (row, generator, solver))
+            print("Run #%d: %s & %s" % (row, generator, solver))
             for col, size in enumerate(SIZES):
                 print(col)
                 setup = """from mazelib import Maze
 from mazelib.solve.%(solv)s import %(solv)s
 from mazelib.generate.%(gen)s import %(gen)s
-""" % {'solv': solver, 'gen': generator}
+""" % {
+                    "solv": solver,
+                    "gen": generator,
+                }
                 logic = """m = Maze()
 m.generator = %(gen)s(%(size)d, %(size)d)
 m.solver = %(solv)s()
 m.generate()
 m.generate_entrances()
 m.solve()
-""" % {'solv': solver, 'gen': generator, 'size': size}
+""" % {
+                    "solv": solver,
+                    "gen": generator,
+                    "size": size,
+                }
                 t = Timer(logic, setup=setup)
                 time = t.timeit(ITERATIONS[col])
                 times[row]
@@ -53,26 +68,28 @@ m.solve()
 
 
 def print_benchmarks(times):
-    """ Pretty print for the benchmark results, with a detailed CSV at the end.
+    """Pretty print for the benchmark results, with a detailed CSV at the end.
 
     Args:
         times (list): timing results for the benchmark runs
     Results: None
     """
-    print('\nmazelib benchmarking')
-    print(datetime.now().strftime('%Y-%m-%d %H:%M'))
-    print('Python version: {0}'.format(get_python_version()))
-    print('mazelib version: {0}'.format(version))
-    print('\nTotal Time (seconds): %.5f\n' % sum([sum(times_row) for times_row in times]))
-    print('\nmaze size,' + ','.join([str(s) for s in SIZES]))
+    print("\nmazelib benchmarking")
+    print(datetime.now().strftime("%Y-%m-%d %H:%M"))
+    print("Python version: {0}".format(get_python_version()))
+    print("mazelib version: {0}".format(version))
+    print(
+        "\nTotal Time (seconds): %.5f\n" % sum([sum(times_row) for times_row in times])
+    )
+    print("\nmaze size," + ",".join([str(s) for s in SIZES]))
 
     row = -1
     for generator in GENERATORS:
         for solver in SOLVERS:
             row += 1
-            method = generator + '-' + solver + ','
-            print(method + ','.join(['%.5f' % time for time in times[row]]))
+            method = generator + "-" + solver + ","
+            print(method + ",".join(["%.5f" % time for time in times[row]]))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-''' How to Install
+""" How to Install
 
 The easiest solution it so install via PyPI:
 
@@ -14,7 +14,7 @@ To run the test suite any of the following will work:
     nosetests test/
     pytest test/
     make test
-'''
+"""
 # non-controversial imports
 from setuptools import setup, find_packages
 from setuptools.command.install import install
@@ -28,63 +28,86 @@ try:
     from distutils.extension import Extension
     from Cython.Distutils import build_ext
     from Cython.Build import cythonize
-    cmdclass = {'install': install, 'build_ext': build_ext}
+
+    cmdclass = {"install": install, "build_ext": build_ext}
     has_cython = True
 except (ModuleNotFoundError, ImportError):
-    print('WARNING: You do not have Cython installed. Installation preceeding without Cython.')
-    cmdclass = {'install': install}
+    print(
+        "WARNING: You do not have Cython installed. Installation preceeding without Cython."
+    )
+    cmdclass = {"install": install}
     has_cython = False
 # You will also need NumPy to compile this Cython
 try:
     import numpy as np
 except (ModuleNotFoundError, ImportError):
-    print('WARNING: You do not have NumPy installed. Installation preceeding without NumPy.')
+    print(
+        "WARNING: You do not have NumPy installed. Installation preceeding without NumPy."
+    )
     has_cython = False
 
 # find all the extension modules in the project, for a Cython build
 if has_cython:
-    FORCE_FLAGS = ['-f', '--f', '--force']
+    FORCE_FLAGS = ["-f", "--f", "--force"]
     FORCE_REBUILD = True if any([f in argv for f in FORCE_FLAGS]) else False
-    IS_WINDOWS = True if (os_name.lower() == 'nt' or 'win' in platform.lower()) else False
-    COMP_DIRS = {'language_level': 3, 'boundscheck': False, 'initializedcheck': False, 'cdivision': True}
-    sep = '\\' if IS_WINDOWS else '/'
-    ext_modules = [Extension(p[:-4].replace(sep, '.'), [p, p[:-2] + 'y'], include_dirs=[np.get_include(), '.'])
-                   for p in glob(sep.join(['mazelib', '*', '*.pxd']))]
-    ext_modules_list = cythonize(ext_modules, annotate=False, force=FORCE_REBUILD, compiler_directives=COMP_DIRS)
+    IS_WINDOWS = (
+        True if (os_name.lower() == "nt" or "win" in platform.lower()) else False
+    )
+    COMP_DIRS = {
+        "language_level": 3,
+        "boundscheck": False,
+        "initializedcheck": False,
+        "cdivision": True,
+    }
+    sep = "\\" if IS_WINDOWS else "/"
+    ext_modules = [
+        Extension(
+            p[:-4].replace(sep, "."),
+            [p, p[:-2] + "y"],
+            include_dirs=[np.get_include(), "."],
+        )
+        for p in glob(sep.join(["mazelib", "*", "*.pxd"]))
+    ]
+    ext_modules_list = cythonize(
+        ext_modules, annotate=False, force=FORCE_REBUILD, compiler_directives=COMP_DIRS
+    )
 else:
     ext_modules_list = []
 
 
 # perform the actual build/install
-setup(cmdclass=cmdclass,
-      name='mazelib',
-      version=__version__,
-      description='A Python API for creating and solving mazes.',
-      url='https://github.com/john-science/mazelib',
-      keywords="game mathematics algorithms maze mazes math games algorithm",
-      author='John Stilley',
-      classifiers=['Development Status :: 4 - Beta',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: Scientific/Engineering :: Mathematics',
-                   'Topic :: Games/Entertainment :: Puzzle Games',
-                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Natural Language :: English'],
-      python_requires='>=3.4, <3.10 ',
-      license='GPLv3',
-      long_description='A Python library for creating and solving mazes.',
-      long_description_content_type='text/markdown',
-      packages=find_packages(),
-      package_data={'mazelib': ['generate/*.pxd', 'solve/*.pxd', 'transmute/*.pxd']},
-      ext_modules=ext_modules_list,
-      platforms='any',
-      test_suite="test",
-      setup_requires=["numpy>=1.13.1,<=1.20.1"],
-      install_requires=["cython>=0.27.0,<=0.29.22",
-                        "numpy>=1.13.1,<=1.20.1"],
-      zip_safe=False)
+setup(
+    cmdclass=cmdclass,
+    name="mazelib",
+    version=__version__,
+    description="A Python API for creating and solving mazes.",
+    url="https://github.com/john-science/mazelib",
+    keywords="game mathematics algorithms maze mazes math games algorithm",
+    author="John Stilley",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.4, <3.10 ",
+    license="GPLv3",
+    long_description="A Python library for creating and solving mazes.",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_data={"mazelib": ["generate/*.pxd", "solve/*.pxd", "transmute/*.pxd"]},
+    ext_modules=ext_modules_list,
+    platforms="any",
+    test_suite="test",
+    setup_requires=["numpy>=1.13.1,<=1.20.1"],
+    install_requires=["cython>=0.27.0,<=0.29.22", "numpy>=1.13.1,<=1.20.1"],
+    zip_safe=False,
+)
